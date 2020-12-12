f74b4a78baa057969615fdd5e6ddff8a55da8c54
Kris Jordaens <kris.jordaens@sde.be>
2020-12-12 15:52:16 +0100

    Day12 Star 2

--- Day12/code.py
+++ Day12/code.py
@@ -5,14 +5,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from AOCHelper import * 
 
 input=[]
+def readinput():
+    global input
+    input = readinput_lines(r"Day12\input.txt")
 
 class Position:
     dirs = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
     ns,ew = 0,0
+    ns_way,ew_way = 1,10
     direction=0
     directions=["E","S","W","N"]
-    def turn(self,dir,degrees):
-        #degrees = int(360 / degrees / 4)
+    def turn(self,dir,degrees):       
         degrees = (degrees // 90)
         if dir=="R":
             self.direction += degrees
@@ -36,12 +39,8 @@ class Position:
             dir = self.dirs[self.directions[self.direction]]
 
         self.ew+=dir[0]*degrees
-        self.ns+=dir[1]*degrees        
-      
-def readinput():
-    global input
-    input = readinput_lines(r"Day12\input.txt")
-   
+        self.ns+=dir[1]*degrees                
+
 def main():
     readinput()
     first_star()
@@ -50,15 +49,35 @@ def main():
 def first_star():  
     pos = Position()   
     for line in input:
-        a,d=line[0],int(line[1:])      
-        pos.move(a,d)
+        action,qty=line[0],int(line[1:])      
+        pos.move(action,qty)
        
     print("Result First Star")   
     print(str(abs(pos.ew) + abs(pos.ns) ))
 
 def second_star():
-   
+    pos = Position()   
+    for line in input:  
+        action,qty=line[0],int(line[1:])        
+
+        if action in pos.dirs:
+            ew, ns = pos.dirs[action]
+            pos.ew_way += qty * ew
+            pos.ns_way += qty * ns
+        else:
+            degrees = (qty // 90)
+            degrees %= 4
+            if action == 'L':
+                degrees = 4 - degrees
+            if action in 'LR':
+                for _ in range(degrees):
+                    pos.ew_way, pos.ns_way = pos.ns_way, - pos.ew_way
+            else:               
+                pos.ew += qty * pos.ew_way
+                pos.ns += qty * pos.ns_way
+     
     print("Result Second Star")
+    print(str(abs(pos.ew) + abs(pos.ns) ))
 
 if __name__ == '__main__':
     main()
