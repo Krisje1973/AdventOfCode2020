fc77c376ba8cbdc3d619e6cbdd8bf299926aca10
Kris Jordaens <kris.jordaens@sde.be>
2020-12-12 21:09:12 +0100

    Refactor day 10

--- AOCHelper.py
+++ AOCHelper.py
@@ -27,18 +27,56 @@ class Compass:
     idx = dirs.index(currentdirection) + degrees
     idx %= len(dirs)
     return (dirs[idx:] + dirs[:idx])[0]       
+class GridHelper:
+  def get_suroundings(self,grid,x,y,count):
+    start =-1
+    end = count -1 + start
+    # Gets a new grid with offset
+    return [grid[y + dy][x + dx] 
     
-def get_suroundings(grid,x,y,count):
-  start =-1
-  end = count -1 + start
-  # Gets a new grid with offset
-  return [grid[y + dy][x + dx] 
-  
-  for dx in range(start, end) 
-  for dy in range(start, end) 
-  if 0 <= y + dy < len(grid) # Check y bounds
-  and 0 <= x + dx < len(grid[x]) # check x bounds
-  and (dx, dy) != (0, 0)]  # not self
+    for dx in range(start, end) 
+    for dy in range(start, end) 
+    if 0 <= y + dy < len(grid) # Check y bounds
+    and 0 <= x + dx < len(grid[x]) # check x bounds
+    and (dx, dy) != (0, 0)]  # not self
  
-def join_lines_from_list(mylist): 
-    return "".join("".join(row) for row in mylist)
+  def join_lines_from_list(self,mylist): 
+      return "".join("".join(row) for row in mylist)
+
+
+  def calculate_combinations(self,data,offset):
+
+    dist = [1]
+    data.sort()
+    for val in range(1, len(data)):
+          total = 0
+          for tot in range(val):
+              if data[tot] + offset >= data[val]:
+                  total += dist[tot]
+          dist.append(total)  
+          
+    return dist
+  
+  def get_int_combinations(self,data,offsets):
+    # =============================================================
+    # Data is list of int's.
+    # Each val in list needs to connect within the offset parameter
+    # =============================================================
+
+    combis = {}
+    combi_vals = {}
+    data.sort()
+    for offset in offsets:
+      combi = []
+      combi_val = []  
+      start = 0
+      for val in data:
+        if val - start == offset:
+          combi.append(1) 
+          combi_val.append(val)  
+        start = val
+      
+      combis[offset] = combi
+      combi_vals[offset] = combi_val      
+         
+    return combis,combi_vals
--- Day10/code.py
+++ Day10/code.py
@@ -5,44 +5,31 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from AOCHelper import * 
 
 input=[]
-joint1 = 0
-joint3 = 0
+grid = GridHelper()
 def readinput():
     global input
-    input = readinput_lines_as_ints(r"Day10\input.txt")
-    input.sort()
+    input = readinput_lines_as_ints(r"Day10\input.txt")   
 def main():
     readinput()
     first_star()
     second_star()        
    
 def first_star():  
-    global joint1,joint3
+    combis = grid.get_int_combinations(input,[1,3])
+    joint1 = sum(combis[0][1])
+    joint3 = sum(combis[0][3])
     joint3 +=1 
-    start = 0
-    for val in input:
-        if val - start == 1:
-            joint1+=1
-        else: 
-            joint3+=1           
-        start = val
     
     print("Result First Star")
     print(str(joint1) + " - " + str(joint3))
     print(str(joint1*joint3))
 
 def second_star():
-    dist = [1]
     data = [0] + input + [input[-1] + 3]
-    for val in range(1, len(data)):
-        total = 0
-        for tot in range(val):
-            if data[tot] + 3 >= data[val]:
-                total += dist[tot]
-        dist.append(total)  
-        
+    data = grid.calculate_combinations(data,3)
+
     print("Result Second Star")
-    print(dist[-1])         
+    print(data[-1])         
 
 if __name__ == '__main__':
     main()
