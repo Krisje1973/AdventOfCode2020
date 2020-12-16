c7a6edd63f0ed31e9bdf1e3d49c34717b4ada0a2
Kris Jordaens <kris.jordaens@sde.be>
2020-12-16 20:25:42 +0100

    Day 16 Star2

--- Day16/code.py
+++ Day16/code.py
@@ -12,7 +12,7 @@ def readinput():
 
 def main():
     readinput()
-    first_star()
+    #first_star()
     second_star()        
 
 def first_star():  
@@ -39,10 +39,46 @@ def first_star():
    
     print("Result First Star")   
     print(sum(notvalid))
+
+def parse_rule(rule):
+    return tuple(map(int, re.findall(r"\d+", rule))), rule.startswith("departure")   
+
 def second_star():
-   
-       
+    helper = FileHelper()
+    rules, ticket, nearby = helper.get_arrays_from_separator(input,'')
+    my_ticket = list(map(int, ticket[0].split(",")))
+    rules = {parse_rule(rule) for rule  in rules}
+
+    # Find valid tickets
+    error_rate = 0
+    valid_tickets = [[] for i in rules]
+    for ticket in nearby:
+        ticket = list(map(int, ticket.split(",")))
+        for val in ticket:
+            if not any(l1 <= val <= u1 or l2 <= val <= u2 for (l1, u1, l2, u2), dep in rules):
+                error_rate += val
+                break
+        else:
+            for ind, val in enumerate(ticket):
+                valid_tickets[ind].append(val)
+
+    tot = 1
+    while rules:
+        for id, ticket_val in enumerate(my_ticket):
+            cand_rules = []
+            for rule in rules:
+                (low1, up1, low2, up2), depart = rule
+                # This check can be replaced using binary search (after sorting)
+                if all(low1 <= val <= up1 or low2 <= val <= up2 for val in valid_tickets[id]):
+                    cand_rules.append(rule)
+            if len(cand_rules) == 1:  # Only 1 rule works
+                rule = cand_rules[0]
+                rules.remove(rule)
+                if rule[1]:
+                    tot *= ticket_val
+
     print("Result Second Star")   
-    
+    print(tot)
+
 if __name__ == '__main__':
     main()
