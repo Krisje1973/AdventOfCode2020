1e6e6e7d4f6e8a89bcf75190b73eae6765defe60
Kris Jordaens <kris.jordaens@sde.be>
2020-12-22 08:46:33 +0100

    Day 21

--- Day20/code.py
+++ Day20/code.py
@@ -11,7 +11,7 @@ def readinput():
     global data
     global tiles
     helper = FileHelper()
-    data = helper.readinput_lines_and_replace(r"Day20\input.txt",[[".","0"],["#","1"]]) 
+    data = helper.readinput_lines_and_replace(r"Day20\input_ex.txt",[[".","0"],["#","1"]]) 
     arr = helper.get_arrays_from_separator(data,"")
     for ar in arr:
         no = ar[0].split(":")[0].replace("Tile ","")
@@ -26,7 +26,7 @@ class Tile():
     no,l,t,r,b,lr,tr,rr,br=[0,0,0,0,0,0,0,0,0]
     lines=[]
     rn,ln,tn,bn=0,0,0,0
-
+    neigborgs = {}
     def calc_borders(self):
         helper = Binary()
         self.t=helper.get_int_from_binary_string(self.lines[0])
@@ -41,7 +41,10 @@ class Tile():
         self.l = helper.get_int_from_binary_string(l)
         self.lr = helper.get_int_from_binary_reversed_string(l)
         self.r = helper.get_int_from_binary_string(r)
-        self.rr = helper.get_int_from_binary_reversed_string(r)        
+        self.rr = helper.get_int_from_binary_reversed_string(r) 
+    def rotate(self):
+        t,tr,r,rr,b,br,l,lr = self.r,self.rr,self.b,self.br,self.l,self.lr,self.t,self.tr
+        self.t,self.tr,self.r,self.rr,self.b,self.br,self.l,self.lr = t,tr,r,rr,b,br,l,lr 
 
 def find_neigborgs(tile):
     corners = [tile.t,tile.b,tile.l,tile.r]
@@ -49,11 +52,12 @@ def find_neigborgs(tile):
         c=tiles[t]      
         for corner in corners:          
             if c.t == corner or c.tr == corner or c.b == corner or c.br == corner or c.l==corner or c.lr == corner or c.r==corner or c.rr==corner: 
-                tile.tn+=1      
+                tile.tn+=1  
+                tile.neigborgs[t]=c.tn  
     
 def main():
     readinput()
-    first_star()
+    #first_star()
     second_star()        
 
 def first_star():  
@@ -63,12 +67,42 @@ def first_star():
         find_neigborgs(tile)       
         if tile.tn==6: 
             tot *= int(t)
-        
+
     print("Result First Star")  
     print(tot) 
    
 def second_star():
+    corners=[]
+    for t in tiles:        
+        tile = tiles[t]
+        find_neigborgs(tile)    
+        if tile.tn==6: 
+           corners.append(t)
+    print(len(tiles))
+    # expected 48 Rows
+    corner = tiles[corners[0]]
+    image = [[]]
+    image[0].append(corner)
+    cnt=0
+    while len(image<9) or cnt == 1000:
+        cnt+=1
+        tils = tiles.copy()
+        tils.pop(corner)
+        for t in tils:
+            if tils[t].
+
+    image[0].append(tile)
+    print(len(image))
+    #while len(image<144):
+
+
+
+    monster = """\
+                    #
+        #    ##    ##    ###
+        #  #  #  #  #  #   """.split('\n')
+
     print("Result Second Star")   
-    
+  
 if __name__ == '__main__':
     main()
--- Day21/code.py
+++ Day21/code.py
@@ -6,24 +6,56 @@ from AOCHelper import *
 import re
 
 data=[]
+cand = defaultdict(list)
+ingr = defaultdict(int)
 def readinput():
     global data
+    global ingr
+    global cand
     data = readinput_lines(r"Day21\input.txt")
-  
-
+    
+    for i in data:
+        ing, allerg = i[:-1].split(" (contains ")
+        for j in allerg.split(", "):
+            cand[j].append(set(ing.split()))
+        for j in ing.split():
+            ingr[j] += 1
+            
 def main():
     readinput()
-    first_star()
+    #first_star()
     second_star()        
 
 def first_star():  
-   
+    uncertain = set()
+    used = set()
+    known = {}
+    for allergen in cand:
+        bad = cand[allergen][0]
+        for i in cand[allergen]:
+            bad &= i
+        uncertain |= bad
+        if len(bad) == 1:
+            known[allergen] = next(iter(bad))
+            used.add(known[allergen])
+
+    while len(cand) != len(known):
+        for allergen in cand:
+            if allergen not in known:
+                bad = cand[allergen][0] - used
+                for i in cand[allergen]:
+                    bad &= i
+                if len(bad) == 1:
+                    known[allergen] = next(iter(bad))
+                    used.add(known[allergen])
+
     print("Result First Star")   
-   
+    print(sum(cnt for i, cnt in ingr.items() if i not in uncertain))
+    return known
 def second_star():
    
-       
+    known = first_star()   
     print("Result Second Star")   
-    
+    print(",".join(j for i, j in sorted(known.items())))
 if __name__ == '__main__':
     main()
