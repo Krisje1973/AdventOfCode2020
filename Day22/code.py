816735bdc2d8d4a72c1ab35d96663d7db9f469af
Kris Jordaens <kris.jordaens@sde.be>
2020-12-22 11:26:10 +0100

    Day 22 WIP

--- Day22/code.py
+++ Day22/code.py
@@ -11,7 +11,7 @@ def readinput():
     global data
     global p1,p2
     helper = FileHelper()   
-    data = readinput_lines(r"Day22\input.txt")
+    data = readinput_lines(r"Day22\input_ex.txt")
     a,b = helper.get_arrays_from_separator(data," ")
     for i in a[1:]:
         p1.append(int(i))
@@ -19,14 +19,12 @@ def readinput():
         p2.append(int(i))
 def main():
     readinput()
-    first_star()
+    #first_star()
     second_star()        
 
 def play():
     global p1,p2   
     while len(p1)>0 and len(p2)>0:       
-        if p2[0] == 10:
-             p2[0] == 10
         if p1[0] > p2[0]:
             p1.append(p1[0])
             p1.append(p2[0])
@@ -35,9 +33,46 @@ def play():
             p2.append(p1[0])
         p1.pop(0)
         p2.pop(0)
-    
-    
 
+def play2(p1,p2):   
+    pd=[]
+    cnt=0
+    while len(p1)>0 and len(p2)>0:
+        cnt+=1
+        # Played before: p1 wins
+        d=p1.copy()
+        d.extend(p2)
+        if pd.count(d):
+            p2.clear()
+            return p1,p2
+        pd.append(d)    
+
+        """
+        # Enough cards
+        if len(p1) < p1[0] or len(p2) < p2[0]:
+            if p1[0] > p2[0]:
+                p2.clear()
+                return p1,p2
+            return p1,p2
+           """
+
+        # Combat 
+        if len(p1) >= p1[0] and len(p2) >= p2[0]:
+            r1,r2 = play2(p1[1:p1[0]+1],p2[1:p2[0]+1])
+            wp= len(r1)>0 
+        else: wp= p1[0] > p2[0]
+
+        if wp:
+            p1.append(p1[0])
+            p1.append(p2[0])
+        else: 
+            p2.append(p2[0])
+            p2.append(p1[0])
+        p1.pop(0)
+        p2.pop(0)
+    
+    return p1,p2
+    
 def first_star():  
     tot = 0
     play()   
@@ -47,12 +82,20 @@ def first_star():
     for i,v in enumerate(p):
         tot += (c-i) * v
 
-    print(tot)
     print("Result First Star")   
-   
+    print(tot)
 def second_star():
-
+    # 29495 to low ...
+    # 36215 to high
+    global p1,p2
+    tot=0
+    p1,p2 = play2(p1,p2)   
+    p=p1
+    if len(p1) == 0: p=p2
+    c=len(p)
+    for i,v in enumerate(p):
+        tot += (c-i) * v
     print("Result Second Star")   
-
+    print(tot)
 if __name__ == '__main__':
     main()
