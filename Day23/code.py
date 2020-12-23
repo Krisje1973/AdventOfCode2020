35198f5b5f4b4dc2ede61bea1ec7611f9d0a39c4
Kris Jordaens <kris.jordaens@sde.be>
2020-12-23 19:32:29 +0100

    Day 23 Almost There

--- Day23/code.py
+++ Day23/code.py
@@ -16,37 +16,126 @@ def main():
     first_star()
     second_star()        
 
-
-def move(times):
+def move2(times):
     global data
-    for i in range(int(times)):
-        pick = data[1:4]      
+    for _ in range(int(times)):
+        pick = data[1:4]
         dest = data[0] - 1 if data[0] > 1 else 9
         while dest in pick:
             dest -= 1
             if dest == 0:
                 dest = 9
-        de = collections.deque(data)
+
+        idx = data.index(dest)
+        if idx == 0:
+            # no change
+            pass
+        else:
+            data = list([data[0]] + data[4 : idx + 1] + pick + data[idx + 1 :])
+
+        data = data[1:] + [data[0]]
+
+def move3(times):
+    global data
+    cur=0
+    for i in range(int(times)):
+        
+        cur=data[cur] 
+        de = collections.deque(data)      
+        while not de[0] == cur:
+            de.rotate(-1)
+        pick = list(de)[1:4]    
+        dest = cur - 1 if cur-1 > 0 else 9
+        while dest in pick:
+            dest -= 1
+            if dest == 0:
+                dest = 9
+        idx = data.index(dest)
+        ch = data.copy()
+        for i in pick:
+            idx= ch.index(i)
+            ch.pop(idx)
+        idx=ch.index(dest)
+        ch =ch[0:idx] + [dest] + pick
+
         while not de[0] == dest:
             de.rotate(-1)
-        #3 (2) 8  9  1  5  4  6  7
-        """
-        -- move 2 --
-        cups:  3 (2) 8  9  1  5  4  6  7 
-        pick up: 8, 9, 1
-        destination: 7
-        result=3  2 (5) 4  6  7  8  9  1
-        -- move 3 --
-        cups:  3  2 (5) 4  6  7  8  9  1 
-        pick up: 4, 6, 7
-        destination: 3
-        result = 7  2  5 (8) 9  1  3  4  6
-        """
-        data = list([data[0]] +[de[0]] + pick + list(de)[1:5])
+        de.rotate(-1)    
+        cur=de[0] 
+        data=ch+list(de)[0:len(data)-len(ch)]
     
+def move(times):
+    global data
+    cur=1
+    for i in range(int(times)):  
+        v= len(data)+i
+        v= v% len(data) 
+        
+        print(i, "--" ,data)
+        curval=data[v] 
+
+        # Rotate so we have a clean list
+        de = collections.deque(data)      
+        while not de[0] == curval:
+            de.rotate(-1)        
+        pick = list(de)[1:4]
+        cur = de[4]
+        dest = curval -1 if curval > 1 else 9
+        while dest in pick:
+            dest-=1
+            if dest==0: dest=9
+        
+        for j,d in enumerate(data):
+            if d==dest:    
+                if j==0:
+                    # expected 7  2  5 (8) 9  1  3  4  6                   
+                    de = collections.deque(data)     
+                    de.rotate(-1)    
+                    data = list(de)
+                    for p in pick:
+                        idx = data.index(p)
+                        data.insert(len(data),data.pop(idx))
+                    de = collections.deque(data)     
+                    de.rotate(1)    
+                    data = list(de)
+                else:
+                    idxf = data.index(pick[0])
+                    if idxf > len(data)-3:
+                       
+                        # pick = 367
+                        # dest = 8 
+                        #[7, 2, 5, 8, 4, 1, 9, 3, 6]
+                        # expected : 8  3  6  7  4  1  9 (2) 5 
+
+                        # 7  4  1  5  8  3  9  2 (6)
+                        offset = len(pick) - (len(data)-idxf)  
+                        de = collections.deque(data)     
+                        de.rotate(-offset)    
+                        data = list(de)
+                        offset = data.index(d) 
+                        for p in pick:
+                            offset+=1
+                            idx = data.index(p)                                
+                            data.insert(offset,data.pop(idx))
+                           
+                        offset = 2 if idxf == 7 else 1   
+                        de = collections.deque(data)     
+                        de.rotate(-offset)    
+                        data = list(de)
+
+                    else:
+                        for p in pick:
+                            idx = data.index(p)
+                            data.insert(j,data.pop(idx))
+               
+               
+       
 
 
-def first_star():    
+
+
+def first_star():   
+   
     move(10)
     print("Result First Star")   
     print(data)
