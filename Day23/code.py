48395dffa5891124203ac10a6c90b9945519fe5f
Kris Jordaens <kris.jordaens@sde.be>
2020-12-26 09:57:31 +0100

    Day23 Star 2

--- Day23/code.py
+++ Day23/code.py
@@ -13,8 +13,12 @@ def readinput():
     
 def main():
     readinput()
-    first_star()
-    second_star()        
+    #first_star()
+    second_star(int(1e6),int(1e7))        
+
+def Move():
+    start=0
+
 
 def move(times):
     global data
@@ -41,13 +45,64 @@ def move(times):
 
 
 def first_star():   
-   
     move(100)
     print("Result First Star")   
     print(data)
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None  
+
+def second_star(N=9,moves=100):
+    # asses: 907135 401051 363807398885
+
    
-def second_star():
-    print("Result Second Star")   
+    cups = {}
+    for c in range(1, N + 1):
+        cups[c] = Node(c)    
+    for x, y in zip(data, data[1:]):
+        x, y = map(int, [x, y])
+        cups[x].next = cups[y]
+
+    if len(data) == N:
+        cups[int(data[-1])].next = cups[int(data[0])]
+    else:
+        cups[int(data[-1])].next = cups[len(data) + 1]
+        for i in range(len(data) + 1, N):
+            cups[i].next = cups[i + 1]
+        cups[N].next = cups[int(data[0])]
+
+    head = cups[int(data[0])]
+    for _ in range(moves):
+        rem_start = head.next
+        rem_end = head.next.next.next
+
+        vals = [rem_start.val, rem_start.next.val, rem_end.val]
+        goal = head.val - 1 if head.val > 1 else N
+        while goal in vals:
+            goal -= 1
+            if goal == 0:
+                goal = N
+
+        head.next = rem_end.next
+        rem_end.next = cups[goal].next
+        cups[goal].next = rem_start
+
+        head = head.next
     
+    if N==9:
+        result=""
+        one = cups[1].next
+        result+=str(one.val)
+        for i in range(7):
+            one = cups[one.next.val]
+            result+=str(one.val)
+
+        print("Result First Star")   
+        print(result)
+
+    print("Result Second Star")   
+    print(cups[1].next.val*cups[1].next.next.val)
 if __name__ == '__main__':
     main()
